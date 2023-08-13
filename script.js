8cbf8e6ee31504223b03ac9903e830de6857dbe3
kajal33cs <124779796+kajal33cs@users.noreply.github.com>
2023-08-13 11:41:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    var par = document.querySelector("#par");
+    let temprature = () => {
+        let opt = document.querySelector("#unit");
+        let display = document.querySelector(".value");
+        var a = document.querySelector(".takval").value;
+    }
